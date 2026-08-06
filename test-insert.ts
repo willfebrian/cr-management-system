5c1a896c48b9c787ee013e6757140ded354dc5fd
bud1purwanto <budi.purwanto15@gmail.com>
2026-08-06 15:13:49 +0700

    chore: remove temporary test scripts and Puppeteer check utility

--- test-insert.ts
+++
@@ -1,4 +0,0 @@
-import { pool } from './src/server/db/pool.ts'; 
-pool.query("INSERT INTO issue_people (full_name, nickname, email, department, is_active, is_approver, is_abaper, is_requester, is_tester, is_evaluator) VALUES ('Test', '', null, 'IT', true, false, false, true, false, false) RETURNING id")
-  .then(res => { console.log(res.rows); process.exit(0); })
-  .catch(console.error);
--- test-nickname.ts
+++
@@ -1,4 +0,0 @@
-import { pool } from './src/server/db/pool.ts'; 
-pool.query("INSERT INTO issue_people (full_name, nickname) VALUES ('Test 2', '')")
-  .then(() => { console.log('success'); process.exit(0); })
-  .catch(console.error);
